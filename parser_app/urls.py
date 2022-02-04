d0bdb3ea2bfcef68dc84e90af886dfce4d2f9397
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2022-02-04 10:04:23 +0300

    hw6

--- parser_app/urls.py
+++ parser_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'parser'
 urlpatterns = [
     path('parser/', views.ParserFormView.as_view(), name='shows_all_p'),
-    path('showss/', views.Shows_p_ListView.as_view(), name='shows_all_p'),
-    path('showss/<int:id>/', views.Shows_p_DetailView.as_view(), name='shows_detail_p'),
+    path('parserShows/', views.Shows_p_ListView.as_view(), name='shows_all_p'),
+    path('parserShows/<int:id>/', views.Shows_p_DetailView.as_view(), name='shows_detail_p'),
 ]
